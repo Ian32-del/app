8f4ae3ffdfe32bc64c3affe7361e3f735c3badbc
Ian Christopher <christopherian96@gmail.com>
2025-02-14 20:18:30 +0300

    Last commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/chris/DevOps/app/venv
+set -gx VIRTUAL_ENV /home/chris/DevOps/app/app/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
